31bb43438f93e44e26b34eaae61a8fc4f3edfb01
lou000 <44552514+lou000@users.noreply.github.com>
2021-10-22 07:44:08 +0200

    lab1

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -6,29 +6,29 @@ import android.widget.Button
 import android.widget.TextView
 import java.lang.NumberFormatException
 
-abstract class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val rollButton: Button = findViewById(R.id.roll_button)
-//        rollButton.setOnClickListener{ rollDice() }
-//
-//        val incrementButton: Button = findViewById(R.id.plus_one)
-//        incrementButton.setOnClickListener { increment() }
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{ rollDice() }
+
+        val incrementButton: Button = findViewById(R.id.plus_one)
+        incrementButton.setOnClickListener { increment() }
     }
 
-//    private fun rollDice(){
-//        val resultText: TextView = findViewById(R.id.roll_text)
-//        val randomInt = (1..6).random()
-//        resultText.text = randomInt.toString()
-//    }
-//
-//    private fun increment(){
-//        val resultText: TextView = findViewById(R.id.roll_text)
-//        val number = try{resultText.text.toString().toInt()} catch (e : NumberFormatException) { 0 }
-//        if (number < 6)
-//            resultText.text = (number + 1).toString()
-//    }
+    private fun rollDice(){
+        val resultText: TextView = findViewById(R.id.roll_text)
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+    }
+
+    private fun increment(){
+        val resultText: TextView = findViewById(R.id.roll_text)
+        val number = try{resultText.text.toString().toInt()} catch (e : NumberFormatException) { 0 }
+        if (number < 6)
+            resultText.text = (number + 1).toString()
+    }
 }
+
